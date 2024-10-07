a6de58e69d2af4bdc5bd3e70a7813c5d41aff585
Yookyung Ji <juliet7403@gmail.com>
2024-10-07 21:23:40 +0900

    Add practice file in 20241007
    다음번에는 7-1. 함수 할 차례~~~

--- practice.py
+++ practice.py
@@ -757,50 +757,187 @@ else:     # else: 위의 조건이 다 안 맞을 때
 # 여기서 에러가 나는데 2회독 나중에 한번 더 확인하기 
 '''
 
-temp = int(input("기온은 어때요? "))
-if 30 <= temp:
-    print("너무 더워용. 나가지 마세용. ")
-elif 10 <= temp and temp < 30:
-    print("괜찮은 날씨에용")
-elif 0 <= temp < 10:
-    print("외투를 챙기세용")
-else: 
-    print("너무 추워용. 나가지 마세용")
+# temp = int(input("기온은 어때요? "))
+# if 30 <= temp:
+#     print("너무 더워용. 나가지 마세용. ")
+# elif 10 <= temp and temp < 30:
+#     print("괜찮은 날씨에용")
+# elif 0 <= temp < 10:
+#     print("외투를 챙기세용")
+# else: 
+#     print("너무 추워용. 나가지 마세용")
 
 
+
+# 20231007
 
 # 6-2. for
 
+# 반복문 for
+'''
+print("대기번호 : 1")
+print("대기번호 : 2")
+print("대기번호 : 3")
+print("대기번호 : 4")
+'''
+
+# for waiting_num in [0, 1, 2, 3, 4]:
+#     print("대기번호 : {0}".format(waiting_num))
+
+# randrange
+for waiting_num in range(5): # 0 에서부터 5 미만까지 # 0, 1, 2, 3, 4
+    print("대기번호 : {0}".format(waiting_num))
+
+for waiting_num in range(1, 6): # 1, 2, 3, 4, 5
+    print("대기번호 : {0}".format(waiting_num))
 
 
+# 스타벅스에 손님이 옴
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니당.".format(customer))
 
 
 
 # 6-3. while
+'''
+# 손님을 불렀는데  5번이나 안오면 커피를 버림
+# while 조건
 
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}, 커피가 준비 되었습니다. {1} 번 남았어용.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기처분되었습니당.")
+'''
+'''
+# 나올 때까지 손님을 부름 
+# while True
 
+customer = "아이언맨"
+index = 1
+while True:
+     print("{0}, 커피가 준비 되었습니당. 호출 {1} 회".format(customer, index)) 
+     index += 1
+# 무한루프에 빠짐. 빠져나가는 법은 ctrl c 누르면 강제 종료
+'''
+'''
+# 커피가 준비된 손님 부르기 / 준비되면 커피주고 안되면 안줌
+customer = "토르"
+person = "UnKnown"
 
-
+while person != customer : # 조건에 만족할 때 까지 계속 반복됨
+    print("{0}, 커피가 준비되었습니당.".format(customer))
+    person = input("이름이 어떻게 되세요? ")
+'''
 
 
 # 6-4. continue 와 break
+# 출석번호가 올라갈 수록 책 읽어봐 시킴
 
+absent = [2, 5] # 2번, 5번 결석
+no_book = [7] # 책을 깜빡하고 안 가져옴
+for student in range(1, 11): # 1,2,3,4,5,6,7,8,9,10
+    if student in absent:
+        continue 
+    elif student in no_book:
+        print("오늘 수업 여기까지. {0}은 교무실로 따라왕!!".format(student))
+        break
+    print("{0}, 책을 읽어봐".format(student))
 
+# continue를 만나게 되면 
+# 밑에 있는 문장을 실행하지 않고 다음 반복을 이어감
+# 즉, 1,3,4,6,7,8,9,10
 
+# 그러나 break를 만나면 반복문 탈출
+# ex) 1,3,4,6 책을 읽어봐 / 오늘 수업 여기까지. 7은 교무실로 따라왕!!
 
+# countinue는 계속해서 다음 반복을 진행
+# break 지금 상황에서 반복문 종료 후 끝냄
 
 
 
 # 6-5.한 줄 for
 
+'''
+# 출석번호가 1, 2, 3, 4, 앞에 100을 붙이기로 함 -> 101, 102, 103, 104.
+students = [1,2,3,4,5]
+print(students)
+students = [i+100 for i in students]
+print(students)
 
+# 학생 이름을 길이로 변환 (띄어쓰기 포함임~)
+students = ["Iron man", "Thor", "I am groot"]
+students = [len(i) for i in students]
+print(students)
 
-
+# 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students)
+'''
 
 
 # 6-6.퀴즈 # 5
+'''
+Quiz) 당신은 Cocoa 서비스를 이용하는 택시 기사님입니다.
+50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오.
+
+조건1 : 승객별 운행 소요 시간은 5분 ~ 50분 사이의 난수로 정해집니다.
+조건2 : 당신은 소요시간 5분 ~ 15분 사이의 승객만 매칭해야 합니다.
+
+(출력문 예제)
+[O] 1번째 손님 (소요시간 : 15분)
+[ ] 2번째 손님 (소요시간 : 50분)
+[O] 3번째 손님 (소요시간 : 5분)
+...
+[ ] 50번째 손님 (소요시간 : 16분)
+
+총 탑승 승객 : 2 분
+'''
+'''
+# 정답 - 어려워서 바로 정답으로 들어감
+from random import *
+cnt = 0 #cnt: count / 총 탑승 승객 수 
+for i in range(1, 51): # 1 ~ 50 이라는 수 (승객)
+    time = randrange(5, 51) # 5분 ~ 50분 소요 시간
+    if 5 <= time <= 15: # 5분 ~ 15분 이내의 손님 (매칭 성공), 탑승 승객 수 증가 처리
+        print ("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        cnt += 1 
+    else: #매칭 실패한 경우
+            print("[] {0}번째 손님 (소요시간 : {1}분".format(i,time))
+
+print("총 탑승 승객 : 2 분")
+'''
 
 
+# 7-1. 함수
+
+
+
+# 7-2. 전달값과 반환값
+
+
+
+# 7-3. 기본값
+
+
+
+# 7-4. 키워드값
+
+
+
+# 7-5. 가변인자
+
+
+
+# 7-6. 지역변수와 전역변수
+
+
+
+# 7-7. 퀴즈 #6
 
 
 
